94f965e7819f131f163a0fc4195d143c65aaf959
jeanetteandrews <jeanette.andrews@gmail.com>
2025-05-06 20:00:42 -0700

    third commit

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log("Hello World!");
+
+let numClicks = 0;
+function updateClickClount() {
+   numClicks = numClicks + 1;
+   console.log(numClicks);
+}
+
+document.body.addEventListener("click", logClickLocation);
+
+function logClickLocation(eventData) {
+	console.log( "User clicked at " + eventData.clientX + ", " + 
+	eventData.clientY );
+}
